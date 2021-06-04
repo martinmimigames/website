4059de72d147318efafda12e9471ca3d47b6c3ee
wmartinmimi <martinmimiw@gmail.com>
2021-06-04 11:52:39 +0800

    test run for mobile

--- res/javascript/default.js
+++ res/javascript/default.js
@@ -1,4 +1,11 @@
 var _isMobile = /Mobi/i.test(window.navigator.userAgent);
 function isMobile(){
        return _isMobile;
-    }
+       }
+function changeCss(res) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = res;
+        document.getElementsByTagName('HEAD')[0].appendChild(link);
+}
